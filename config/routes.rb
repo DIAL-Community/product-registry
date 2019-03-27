c278571eb0099a4fbd61738b535d151da13d5eb0
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-03-27 10:54:51 -0400

    Adding nested routes for each model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'organizations#map'
-  resources :sectors
-  resources :locations
-  resources :contacts
   resources :products
-  resources :organizations
+
+  resources :organizations do
+    resources :contacts
+    resources :locations
+    resources :sectors
+  end
+
+  resources :locations do
+    resources :organizations
+  end
+
+  resources :sectors do
+    resources :organizations
+  end
+
+  resources :contacts do
+    resources :organizations
+  end
+
   get 'map', :to => 'organizations#map'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
