2b0b253f677426a76c1f88b56856d0016f74fd31
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-03-28 16:06:39 -0400

    Removing unused fields from create contact.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -101,6 +101,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :slug, :email, :title, :selected_organizations, organizations_attributes: [:id])
+      params.require(:contact).permit(:name, :slug, :email, :title, :selected_organizations)
     end
 end
