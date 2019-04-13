f2b5f1a94c3a255e908b3362cc7a43166109fa15
David McCann <david.a.mccann@gmail.com>
2019-04-13 15:01:40 -0400

    Add product-to-product m2m relationship to schema and model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,13 @@ class Product < ApplicationRecord
   has_one :product_assessment
   has_and_belongs_to_many :organizations
   has_and_belongs_to_many :building_blocks, join_table: :products_building_blocks
+
+  has_many :target_product_rel, foreign_key: :from_product_id, class_name: 'ProductProductRelationship'
+  has_many :includes, through: :target_product_rel, source: :include
+
+  has_many :source_product_rel, foreign_key: :to_product_id, class_name: 'ProductProductRelationship'
+  has_many :references, through: :source_product_rel, source: :reference
+
   validates :name,  presence: true, length: { maximum: 300 }
 
   scope :starts_with, -> (name) { where("LOWER(name) like LOWER(?)", "%#{name}%")}
