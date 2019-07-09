0934a50893fcdb60c0f800fa87b8fa5f0e677327
Steve Conrad <sconrad@digitalimpactalliance.org>
2019-07-09 08:59:37 -0500

    Add role to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+
+  enum role: [:admin, :ict4sdg, :principle, :user]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
