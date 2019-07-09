5485312aba83f8831b60b3148b79d30d8929e6ca
David McCann <david.a.mccann@gmail.com>
2019-07-09 11:27:07 -0400

    don't allow anonymous edits of BBs, use cases, workflows

--- app/controllers/sdg_targets_controller.rb
+++ app/controllers/sdg_targets_controller.rb
@@ -1,5 +1,6 @@
 class SdgTargetsController < ApplicationController
   before_action :set_sdg_target, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     if params[:without_paging]
