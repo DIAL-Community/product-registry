b8a39c51703a054a1d1e24436d710abc3bcf1ef6
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-07-17 17:41:11 -0400

    Adding unit with slug function change.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def slug_em(input)
     slug = input
       .split(%r{\s+})
-      .map{|part| part.gsub(/[^A-Za-z]/, "").downcase}
+      .map{|part| part.gsub(/[^A-Za-z0-9]/, "").downcase}
       .join("_")
     if (slug.length > 32)
       slug = slug.slice(0, 32)
