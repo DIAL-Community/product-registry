b3262499fc63b093c442ef3b2dd71881a81d23ae
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-07-18 14:51:54 +0000

    Adding the ict4sdg folks.

--- app/policies/deploy_policy.rb
+++ app/policies/deploy_policy.rb
@@ -7,7 +7,7 @@ class DeployPolicy < ApplicationPolicy
   end
 
   def mod_allowed?
-    user.role == "admin" || user.role == "principle"
+    user.role == "admin" || user.role == "principle" || user.role == "ict4sdg"
   end
 
   def view_allowed?
