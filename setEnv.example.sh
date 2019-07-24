b52251f86eaef6bbca62b859087275a119be1abb
Steve Conrad <sconrad@digitalimpactalliance.org>
2019-07-24 19:47:17 +0000

    Secret mgmt

---
+++ setEnv.example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SECRET_KEY_BASE=<devise secret key>
+export DATABASE_NAME=registry_development
+export DATABASE_USER=registry
+export DATABASE_PASSWORD=<DB password>
+export ESRI_CLIENT_ID=<esri client id>
+export ESRI_CLIENT_SECRET=<esri secret>
+export JENKINS_URL=<jenkins host url>
+export JENKINS_USER=<jenkins user>
+export JENKINS_PASSWORD=<jenkins password>
+export MAILGUN_API_KEY=<mailgun api key>
+export MAILGUN_DOMAIN=<mailgun domain>
