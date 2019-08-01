dcae625fff13af347dc7efa133008a5ed431dc72
Steve Conrad <sconrad@digitalimpactalliance.org>
2019-08-01 10:05:44 -0500

    Configure which admin users receive backups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190730155346) do
+ActiveRecord::Schema.define(version: 20190731195112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -349,6 +349,7 @@ ActiveRecord::Schema.define(version: 20190730155346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 3, null: false
+    t.boolean "receive_backup", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
