1d8c8ae4b7727c555f0800da09d4386b2b0dd68f
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-08-01 13:53:14 -0400

    Allowing to optionally finding organization by id.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -254,9 +254,8 @@ class OrganizationsController < ApplicationController
 
     # Use callbacks to share common setup or constraints between actions.
     def set_organization
-      if !params[:id].scan(/\D/).empty?
-        @organization = Organization.find_by(slug: params[:id])
-      else
+      @organization = Organization.find_by(slug: params[:id])
+      if @organization.nil? && params[:id].scan(/\D/).empty?
         @organization = Organization.find(params[:id])
       end
     end
