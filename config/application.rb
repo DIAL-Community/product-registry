5592470314e7332353458abc3270a612190d51f2
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-08-08 09:57:24 -0400

    Adding gem to read user language preferences.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Registry
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    config.i18n.available_locales = %w(en es fr)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
