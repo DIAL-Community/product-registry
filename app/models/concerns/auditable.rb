ba0bcebd78836024841ce9326b2c421f6cac13b3
Steve Conrad <sconrad@digitalimpactalliance.org>
2019-09-10 17:42:23 -0500

    Initial implementation of audit logs for orgs

---
+++ app/models/concerns/auditable.rb
@@ -0,0 +1,52 @@
+module Auditable
+  extend ActiveSupport::Concern
+
+  included do
+    after_save { create_audit("UPDATED") }
+    before_destroy { create_audit("DELETED") }
+  end
+
+  def create_audit(action)
+    audit_log = Audit.new
+    audit_log.associated_type = self.class.name
+    audit_log.associated_id = self.slug
+    audit_log.action = action
+    if action == "UPDATED" && self.id_changed?
+      audit_log.action = "CREATED"
+    end
+    current_user = get_current_user
+    audit_log.user_id = current_user.id
+    audit_log.user_role = current_user.role
+    audit_log.username = current_user.email
+    audit_changes = []
+    if !self.saved_changes.empty?
+      audit_changes.push(self.saved_changes)
+    end
+    if !association_changes.nil?
+      audit_changes.push(self.association_changes)
+    end
+    if (!get_image_changed.nil?)
+      audit_changes.push({"image":get_image_changed.to_s})
+    end
+    if !audit_changes.empty? || audit_log.action == "DELETED"
+      audit_log.audit_changes = audit_changes
+      audit_log.save
+    end
+  end
+
+  def set_current_user(user)
+    @current_user = user
+  end
+
+  def get_current_user
+    @current_user
+  end
+
+  def set_image_changed(filename)
+    @image_changed = filename
+  end
+
+  def get_image_changed
+    @image_changed
+  end
+end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,14 +1,51 @@
-class Organization < ApplicationRecord
-  has_and_belongs_to_many :products, join_table: :organizations_products
-  has_and_belongs_to_many :locations, join_table: :organizations_locations
-  has_and_belongs_to_many :contacts, join_table: :organizations_contacts
-  has_and_belongs_to_many :sectors, join_table: :organizations_sectors
-  has_and_belongs_to_many :projects, join_table: :projects_organizations, dependent: :delete_all
+class Organization < ApplicationRecord 
+  include Auditable
 
+  has_and_belongs_to_many :products, join_table: :organizations_products, after_add: :association_add, before_remove: :association_remove
+  has_and_belongs_to_many :locations, join_table: :organizations_locations, after_add: :association_add, before_remove: :association_remove
+  has_and_belongs_to_many :contacts, join_table: :organizations_contacts, after_add: :association_add, before_remove: :association_remove
+  has_and_belongs_to_many :sectors, join_table: :organizations_sectors, after_add: :association_add, before_remove: :association_remove
+  has_and_belongs_to_many :projects, join_table: :projects_organizations, dependent: :delete_all, after_add: :association_add, before_remove: :association_remove
+  
   validates :name,  presence: true, length: { maximum: 300 }
 
   scope :name_contains, -> (name) { where("LOWER(organizations.name) like LOWER(?)", "%#{name}%")}
   scope :slug_starts_with, -> (slug) { where("LOWER(organizations.slug) like LOWER(?)", "#{slug}\\_%")}
+
+  def association_add(new_obj)
+    initialize_association_changes
+    curr_change = {id: new_obj.slug, action: "ADD"}
+    log_association(curr_change, new_obj.class.name)
+  end
+
+  def association_remove(old_obj)
+    initialize_association_changes
+    curr_change = {id: old_obj.slug, action: "REMOVE"}
+    log_association(curr_change, old_obj.class.name)
+  end
+
+  def log_association(curr_change, obj_name)
+    case obj_name
+    when "Sector"
+      @association_changes[:sectors] << curr_change
+    when "Product"
+      @association_changes[:products] << curr_change
+    when "Location"
+      @association_changes[:locations] << curr_change
+    when "Contact"
+      @association_changes[:contacts] << curr_change
+    when "Project"
+      @association_changes[:projects] << curr_change
+    end
+  end
+
+  def initialize_association_changes
+    @association_changes = {sectors: [], products: [], locations: [], contacts: [], projects: []} if @association_changes.nil?
+  end
+
+  def association_changes
+    @association_changes
+  end
 
   def image_file
     if File.exist?(File.join('app','assets','images','organizations',"#{slug}.png"))
