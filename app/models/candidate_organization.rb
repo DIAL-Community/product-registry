037ef8ceadaf90a926c9aad0105f085abd69592e
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-09-13 15:16:28 -0400

    Changing to correct db object.

--- app/models/candidate_organization.rb
+++ app/models/candidate_organization.rb
@@ -3,8 +3,8 @@ class CandidateOrganization < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 300 }
 
-  scope :name_contains, ->(name) { where('LOWER(organizations.name) like LOWER(?)', "%#{name}%") }
-  scope :slug_starts_with, ->(slug) { where('LOWER(organizations.slug) like LOWER(?)', "#{slug}\\_%") }
+  scope :name_contains, ->(name) { where('LOWER(candidate_organizations.name) like LOWER(?)', "%#{name}%") }
+  scope :slug_starts_with, ->(slug) { where('LOWER(candidate_organizations.slug) like LOWER(?)', "#{slug}\\_%") }
 
   def to_param
     slug
