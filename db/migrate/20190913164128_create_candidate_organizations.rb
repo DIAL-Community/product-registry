59f7a625439c22230a066e2d976c79ea8b6fa7ab
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2019-09-13 16:52:15 -0400

    Adding rejected field in the db.

--- db/migrate/20190913164128_create_candidate_organizations.rb
+++ db/migrate/20190913164128_create_candidate_organizations.rb
@@ -1,9 +1,13 @@
 class CreateCandidateOrganizations < ActiveRecord::Migration[5.1]
   def change
     create_table :candidate_organizations do |t|
-      t.string :name
-      t.string :slug
+      t.string :name, null: false
+      t.string :slug, unique: true, null: false
       t.text :website
+
+      t.boolean :rejected
+      t.datetime :rejected_date
+      t.references :rejected_by, index: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
