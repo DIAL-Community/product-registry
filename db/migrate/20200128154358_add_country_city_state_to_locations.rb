c32426946aa5abc4793c5cfb95df4073613c8cd6
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2020-02-03 20:25:21 +0000

    Cleanup location

---
+++ db/migrate/20200128154358_add_country_city_state_to_locations.rb
@@ -0,0 +1,7 @@
+class AddCountryCityStateToLocations < ActiveRecord::Migration[5.1]
+  def change
+    add_column :locations, :country, :string
+    add_column :locations, :city, :string
+    add_column :locations, :state, :string
+  end
+end
