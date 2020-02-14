f9e75ce2b57a3df4487251005fd6685d6e8b6d07
Steve Conrad <sconrad@digitalimpactalliance.org>
2020-02-14 14:15:40 -0600

    Fix unit test

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -27,7 +27,7 @@ class Organization < ApplicationRecord
   has_many :organizations_contacts
   has_many :contacts, through: :organizations_contacts
   has_many :organizations_products
-  has_many :contacts, through: :organizations_products
+  has_many :products, through: :organizations_products
   has_many :organization_descriptions
 
   validates :name, presence: true, length: { maximum: 300 }
