faed78e68b73e072401387f419de89d0587f8091
Steve Conrad <sconrad@digitalimpactalliance.org>
2020-02-25 14:43:25 -0600

    Update Dockerfile with new publicgoods repo

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ git pull
 
 cd /t4d
 rake sync:unicef_goods['/candidates/nominees']
-rake sync:public_goods['/products/products']
+# rake sync:public_goods['/products/products']
 rake sync:digi_square_digital_good
 rake sync:osc_digital_good
 rake sync:purge_removed_products['/candidates/nominees']
