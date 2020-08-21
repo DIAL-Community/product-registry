929dd5d49367ec8224726814fb300e6724111b71
sconrad <sconrad@digitalimpactalliance.org>
2020-08-21 14:08:25 -0500

    Fix for failing migration

--- db/migrate/20200811150942_add_column_roles_to_users.rb
+++ db/migrate/20200811150942_add_column_roles_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnRolesToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column(:users, :roles, :user_role, array: true, default: [])
+    add_column(:users, :roles, :user_role, array: true)
     # change_column_null(:users, :role, true)
   end
 end
