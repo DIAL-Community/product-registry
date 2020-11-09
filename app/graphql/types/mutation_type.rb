c88c342258daea0bc488c4df37492038f665f7e8
Steve Conrad <sconrad@digitalimpactalliance.org>
2020-11-09 14:57:17 +0000

    Updates to GraphQL schema for playbooks and pages

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_activity, mutation: Mutations::CreateActivity
+    field :create_page, mutation: Mutations::CreatePage
     field :register_user, mutation: Mutations::RegisterUser
     field :sign_in, mutation: Mutations::SignIn
     #field :sign_out, mutation: Mutations::SignOut
