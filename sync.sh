f274c14b0fedcd7d8e7670f84f71579c6b47009d
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-01-25 08:10:50 -0600

    Remove deleted products task fron nightly sync

--- sync.sh
+++ sync.sh
@@ -14,7 +14,6 @@ cd /t4d
 rake sync:public_goods['/products/products']
 rake sync:digi_square_digital_good
 rake sync:osc_digital_good
-rake sync:purge_removed_products['/candidates/nominees']
 rake sync:update_version_data
 rake sync:update_license_data
 rake sync:update_statistics_data
