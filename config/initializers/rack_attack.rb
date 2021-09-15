236049f45dfbc2de90b676ae1f31652e7bfa6f28
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-09-15 08:44:44 -0500

    Disable rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,19 +10,19 @@ catalog_ux_routes = [
   '/graphql'
 ]
 
-Rack::Attack.throttle('Catalog UX routes.', limit: 10, period: 1.minute) do |request|
-  request.ip if catalog_ux_routes.any? { |route| request.path.include?(route) } && request.post?
-end
+#Rack::Attack.throttle('Catalog UX routes.', limit: 10, period: 1.minute) do |request|
+#  request.ip if catalog_ux_routes.any? { |route| request.path.include?(route) } && request.post?
+#end
 
-Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+#Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
-Rack::Attack.blocklisted_response = lambda do |_request|
+#Rack::Attack.blocklisted_response = lambda do |_request|
   # Using 503 because it may make attacker think that they have successfully
   # DOSed the site. Rack::Attack returns 403 for blocklists by default
-  [503, {}, ['Server Error.']]
-end
+#  [503, {}, ['Server Error.']]
+#end
 
-Rack::Attack.throttled_response = lambda do |_request|
+#Rack::Attack.throttled_response = lambda do |_request|
   # NB: you have access to the name and other data about the matched throttle
   #  request.env['rack.attack.matched'],
   #  request.env['rack.attack.match_type'],
@@ -31,5 +31,5 @@ Rack::Attack.throttled_response = lambda do |_request|
 
   # Using 503 because it may make attacker think that they have successfully
   # DOSed the site. Rack::Attack returns 429 for throttling by default
-  [503, {}, ["Server Error."]]
-end
+#  [503, {}, ["Server Error."]]
+#end
