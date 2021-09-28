a1e8b635728a81fae51fb9177780a9e4d0176d37
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2021-09-28 19:58:01 +0000

    Fix use case step and fix api route in nginx config.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -10,7 +10,7 @@ class ErrorsController < ApplicationController
 
   def server_error
     respond_to do |format|
-      format.html { render(template: 'errors/internal_server', layout: 'layouts/error', status: 500) }
+      format.html { render(template: 'errors/server_error', layout: 'layouts/error', status: 500) }
       format.all  { render(nothing: true, status: 500) }
     end
   end
