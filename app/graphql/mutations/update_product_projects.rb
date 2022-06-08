f8113562085be7810388ee581f7b8675723cc038
Nikola Laskowska <nlaskowska@soldevelo.com>
2022-06-08 21:08:52 +0000

    CDS-574: Added mutation to update product projects

---
+++ app/graphql/mutations/update_product_projects.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Mutations
+  class UpdateProductProjects < Mutations::BaseMutation
+    argument :projects_slugs, [String], required: true
+    argument :slug, String, required: true
+
+    field :product, Types::ProductType, null: true
+    field :errors, [String], null: true
+
+    def resolve(projects_slugs:, slug:)
+      product = Product.find_by(slug: slug)
+
+      unless an_admin || a_product_owner(product.id)
+        return {
+          product: nil,
+          errors: ['Must be admin or product owner to update a product']
+        }
+      end
+
+      product.projects = []
+      if !projects_slugs.nil? && !projects_slugs.empty?
+        projects_slugs.each do |project_slug|
+          current_project = Project.find_by(slug: project_slug)
+          product.projects << current_project unless current_project.nil?
+        end
+      end
+
+      if product.save
+        # Successful creation, return the created object with no errors
+        {
+          product: product,
+          errors: []
+        }
+      else
+        # Failed save, return the errors to the client
+        {
+          product: nil,
+          errors: product.errors.full_messages
+        }
+      end
+    end
+  end
+end
