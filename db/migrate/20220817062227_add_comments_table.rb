e91609231e3002046fd56fe25b2606d17670c245
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2022-09-01 13:12:39 +0000

    Updating the change to up and down step to ensure rollback.

--- db/migrate/20220817062227_add_comments_table.rb
+++ db/migrate/20220817062227_add_comments_table.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AddCommentsTable < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table(:comments) do |t|
       t.integer(:comment_object_id, null: false)
       t.jsonb(:author, null: false)
@@ -13,5 +13,9 @@ class AddCommentsTable < ActiveRecord::Migration[5.2]
     end
 
     add_column(:comments, :comment_object_type, :comment_object_type, null: false)
+  end
+
+  def down
+    drop_table(:comments)
   end
 end
